e6900cce7e2f9f540c769171d927e67ead7284f2
yinqianye <86508963+yinqianye@users.noreply.github.com>
2025-08-24 10:03:05 +0800

    尝试升级mod至1.21.8
    升级了fabric-api版本和gradle版本，修复了因方法更改导致的崩溃问题，仅剩在Dg_labClient文件中onHudRender函数内的强度显示问题（经测试渲染，方法本身无问题，可能是内部逻辑错误）

--- src/client/java/online/kbpf/dg_lab/client/screen/WaveformScreen/Custom/CustomSliderWidget.java
+++ src/client/java/online/kbpf/dg_lab/client/screen/WaveformScreen/Custom/CustomSliderWidget.java
@@ -1,13 +1,11 @@
 package online.kbpf.dg_lab.client.screen.WaveformScreen.Custom;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.SliderWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
-import org.objectweb.asm.tree.analysis.Value;
 
 import static online.kbpf.dg_lab.client.screen.WaveformScreen.Custom.CustomScreen.list;
 
@@ -23,30 +21,57 @@ public abstract class CustomSliderWidget extends SliderWidget {
         super(x, y, width, height, text, value);
     }
 
-    public void setValue(int value){
-
+    public void setValue(int value) {
         this.setMessage(Text.literal(String.valueOf(value)));
         this.value = (double) value / 100;
     }
 
     @Override
     public void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
-
         MinecraftClient minecraftClient = MinecraftClient.getInstance();
-        context.setShaderColor(1.0F, 1.0F, 1.0F, this.alpha);
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.enableDepthTest();
-        context.drawGuiTexture(this.getTexture(), this.getX(), this.getY(), this.getWidth(), this.getHeight());
-        context.drawGuiTexture(this.getHandleTexture(), this.getX() + (int)(this.value * (double)(this.width - 8)), this.getY(), 8, this.getHeight());
-        context.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+
+        // 使用预定义的 RenderPipeline - 从源码中可以看到有 RenderPipelines.GUI
+        com.mojang.blaze3d.pipeline.RenderPipeline renderPipeline = net.minecraft.client.gl.RenderPipelines.GUI;
+
+        // 滑块背景
+        context.drawTexture(
+                renderPipeline,              // RenderPipeline
+                this.getTexture(),           // Identifier texture
+                this.getX(),                 // int x
+                this.getY(),                 // int y
+                0.0F,                        // float u
+                0.0F,                        // float v
+                this.getWidth(),             // int width
+                this.getHeight(),            // int height
+                this.getWidth(),             // int regionWidth
+                this.getHeight(),            // int regionHeight
+                0                            // int z
+        );
+
+        // 滑块手柄
+        context.drawTexture(
+                renderPipeline,              // RenderPipeline
+                this.getHandleTexture(),     // Identifier texture
+                this.getX() + (int)(this.value * (double)(this.width - 8)), // int x
+                this.getY(),                 // int y
+                0.0F,                        // float u
+                0.0F,                        // float v
+                8,                           // int width
+                this.getHeight(),            // int height
+                8,                           // int regionWidth
+                this.getHeight(),            // int regionHeight
+                0                            // int z
+        );
+
         int i = this.active ? 16777215 : 10526880;
-//        this.drawScrollableText(context, minecraftClient.textRenderer, 2, i | MathHelper.ceil(this.alpha * 255.0F) << 24);
-        context.drawTextWithShadow(minecraftClient.textRenderer, this.getMessage(), this.getX() + this.getWidth(), this.getY(), i | MathHelper.ceil(this.alpha * 255.0F) << 24);
-
-
+        context.drawTextWithShadow(
+                minecraftClient.textRenderer,
+                this.getMessage(),
+                this.getX() + this.getWidth(),
+                this.getY(),
+                i | MathHelper.ceil(this.alpha * 255.0F) << 24
+        );
     }
-
 
     private Identifier getTexture() {
         return this.isFocused() && !this.sliderFocused ? HIGHLIGHTED_TEXTURE : TEXTURE;
@@ -56,14 +81,9 @@ public abstract class CustomSliderWidget extends SliderWidget {
         return !this.hovered && !this.sliderFocused ? HANDLE_TEXTURE : HANDLE_HIGHLIGHTED_TEXTURE;
     }
 
-
-
-
     @Override
     protected abstract void updateMessage();
 
     @Override
     protected abstract void applyValue();
-
-
 }
